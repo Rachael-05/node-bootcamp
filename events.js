2002c99fbf01e4aa95009ec7e8e3137ce399d053
Rachael-05 <rachhall05@gmail.com>
2026-04-30 12:17:08 -0400

    4/30

--- events.js
+++ events.js
@@ -1,6 +1,13 @@
-const EvetEmitter = require("events");
+const EventEmitter = require("events");
+const http = require("http");
 
-const myEmitter = new EvetEmitter();
+class Sales extends EventEmitter {
+  constructor() {
+    super();
+  }
+}
+
+const myEmitter = new Sales();
 
 myEmitter.on("newSale", () => {
   console.log("There was a new sale");
@@ -10,4 +17,28 @@ myEmitter.on("newSale", () => {
   console.log("Customer name: Jonas");
 });
 
-myEmitter.emit("newSale");
+myEmitter.on("newSale", (stock) => {
+  console.log(`There are now ${stock} items left in stock`);
+});
+
+myEmitter.emit("newSale", 9);
+
+///////////////////////////
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  console.log("Request received!");
+  res.end("Request received");
+});
+
+server.on("request", (req, res) => {
+  res.end("another request");
+});
+
+server.on("close", () => {
+  console.log("Server closed");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Waiting for requests...");
+});
