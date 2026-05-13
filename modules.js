3cbfc292182f1c41449852a386134e6e979a29d7
Rachael-05 <rachhall05@gmail.com>
2026-05-13 12:19:02 -0400

    end section.5/13

--- modules.js
+++ modules.js
@@ -7,5 +7,11 @@ const calc1 = new C();
 console.log(calc1.add(2, 5));
 
 //exports
-const calc2 = require("./test-module-2");
-console.log(calc2.multiply(2, 5));
+// const calc2 = require("./test-module-2");
+const { add, multiply } = require("./test-module-2");
+console.log(multiply(2, 5));
+
+//Caching
+require("./test-module-3")();
+require("./test-module-3")();
+require("./test-module-3")();
